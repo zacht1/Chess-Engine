00bffaf841608f7c1fd3180d4bf0d4fe72c7cb98
zach <taylorzachary8@gmail.com>
2022-08-10 16:04:34 -0700

    Specify Zobrist class
    This commit specifies all necessary methods and fields in the zobrist class, which hashes board positions efficiently

---
+++ README.md
@@ -0,0 +1,5 @@
+# Chess Engine
+
+### Sources & Acknowledgments
+- General Info: https://www.chessprogramming.org/Main_Page
+- Zobrist Hashing: https://levelup.gitconnected.com/zobrist-hashing-305c6c3c54d0
---
+++ src/main/model/Zobrist.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Zobrist {
+    private long[][][] table = new long[8][8][12];
+
+    /**
+     * Initializes the Zobrist class
+     */
+    public Zobrist() {
+        // stub
+    }
+
+    /**
+     * Initializes the Zobrist hash table
+     */
+    private void initTable() {
+        // stub
+    }
+
+    /**
+     * Generates a random number from 0 to 2^64-1
+     */
+    private long randomLongGenerator() {
+        return 0L; // stub
+    }
+
+    /**
+     * Compute the hash value of the given board
+     */
+    public long calculateHash(int[] board) {
+        return 0L; // stub
+    }
+
+
+}
